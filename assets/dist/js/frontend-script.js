9b1cc9193c4b24831100e1380b9c2b47bb29b7d9
raftaar1191 <raftaar1191@gmail.com>
2023-11-08 14:10:25 +0530

    remove and add parent class with js

--- assets/dist/js/frontend-script.js
+++ assets/dist/js/frontend-script.js
@@ -8,10 +8,13 @@ jQuery( document ).ready( function($) {
 
 		var message_modal   = $( '#bb-pin-comment-confirmation-modal' );
 
-        var activity_id = $(this).parents( "li" ).attr( "data-bp-activity-comment-id" );
+        var parent_li = $(this).parents( "li" )
+
+        var activity_id = parent_li.attr( "data-bp-activity-comment-id" );
         var url = pin_comment_object.unpin_url;
         var comment_class = 'pin-activity-comment';
         var comment_text = pin_comment_object.pin_text;
+        var parent_class = '_unpin_comment';
 
         /**
          * Check if the user is try to pin the comment
@@ -20,6 +23,7 @@ jQuery( document ).ready( function($) {
             url = pin_comment_object.pin_url;
             var comment_class = 'unpin-activity-comment';
             var comment_text = pin_comment_object.unpin_text;
+            var parent_class = '_pin_comment';
         }
 
         url = url + activity_id + '/';
@@ -31,14 +35,29 @@ jQuery( document ).ready( function($) {
                 xhr.setRequestHeader('X-WP-Nonce', pin_comment_object.nonce );
             },
             success: function(response) {
+
+                /**
+                 * Show popup
+                 */
                 message_modal.find('.bb-action-popup-content').html( response.feedback );
                 message_modal.show();
 
+                /**
+                 * Update the class on the Button
+                 */
                 target.removeClass( 'pin-activity-comment' );
                 target.removeClass( 'unpin-activity-comment' );
 
                 target.addClass( comment_class );
                 target.find( 'span' ).text( comment_text );
+
+
+                /**
+                 * Update the li class
+                 */
+                parent_li.removeClass( '_pin_comment' );
+                parent_li.removeClass( '_unpin_comment' );
+                target.addClass( parent_class );
             }
         });
     });
